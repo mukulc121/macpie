36db7b5b660117a13ce2d00425303083936de4af
Mukul <mukul.chaturvedi@headout.com>
2025-08-13 01:13:45 +0530

    Initial Commit

---
+++ MacPie/MacPieApp.swift
@@ -0,0 +1,17 @@
+//
+//  MacPieApp.swift
+//  MacPie
+//
+//  Created by Mukul on 13/08/25.
+//
+
+import SwiftUI
+
+@main
+struct MacPieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MacPieTests/MacPieTests.swift
@@ -0,0 +1,17 @@
+//
+//  MacPieTests.swift
+//  MacPieTests
+//
+//  Created by Mukul on 13/08/25.
+//
+
+import Testing
+@testable import MacPie
+
+struct MacPieTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
